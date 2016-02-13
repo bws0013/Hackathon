c140a6da53cd3cfc081c2e174c52e14d7e100ab8
William Hester <williamehester@gmail.com>
2016-02-13 09:34:14 -0600

    Create Scraper project

---
+++ Scraper/src/me/williamhester/Main.java
@@ -0,0 +1,13 @@
+package me.williamhester;
+
+public class Main {
+
+    /**
+     * Our worker loop for the scraper. Any API requests should be funneled through here.
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Scraper/src/me/williamhester/network/InstagramApi.java
@@ -0,0 +1,10 @@
+package me.williamhester.network;
+
+/**
+ * Created by william on 2/13/16.
+ */
+public class InstagramApi {
+
+
+
+}
