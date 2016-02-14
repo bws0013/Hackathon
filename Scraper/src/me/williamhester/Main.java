6a95e5085494c7fdfedd4ce24508fc7abd3d91fd
John Harrison <Mac@Johns-MacBook-Pro.local>
2016-02-13 20:52:05 -0600

    This is a commit message

--- Scraper/src/me/williamhester/Main.java
+++ Scraper/src/me/williamhester/Main.java
@@ -1,5 +1,16 @@
 package me.williamhester;
 
+import me.williamhester.network.TwitterApi;
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     /**
@@ -7,7 +18,98 @@ public class Main {
      *
      * @param args not used
      */
-    public static void main(String[] args) {
+    public static void main(String args[]) throws Exception{
+        TwitterApi.setAccount();
+        TwitterApi.getUser("John Harrison");
+        //TwitterApi.getLimit();
+        twitterTest6();
+    }
 
+    private static void twitterTest1() throws IOException, TwitterException {
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("YvHpP4i1s3LNq5S1gqGTyE5TU", "SZKksZSJMraTIloGYJHO597E0b2MwcMjp12N0p0q4hZmVozQqF");
+        RequestToken requestToken = twitter.getOAuthRequestToken();
+        AccessToken accessToken = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (null == accessToken) {
+            System.out.println("Open the following URL and grant access to your account:");
+            System.out.println(requestToken.getAuthorizationURL());
+            System.out.println("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+            String pin = br.readLine();
+            try {
+                if (pin.length() > 0) {
+                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                } else {
+                    accessToken = twitter.getOAuthAccessToken();
+                }
+            } catch (TwitterException te) {
+                if (401 == te.getStatusCode()) {
+                    System.out.println("Unable to get the access token.");
+                } else {
+                    te.printStackTrace();
+                }
+            }
+        }
+        //persist to the accessToken for future reference.
+        //storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
+    }
+
+    private static void twitterTest2() {
+        ArrayList<String> user = new ArrayList<>(TwitterApi.getUser("John Harrison"));
+        for (String s : user) {
+            System.out.println(s);
+        }
+    }
+
+    private static void twitterTest3() {
+        TwitterApi.postTweet("Posting from Auburn's Equifax Hackathon!");
+    }
+
+    private static void twitterTest4() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter the name of the person's birthday below:");
+        String name = in.readLine();
+        List<String> user = TwitterApi.findBirthday(name);
+        for (String s : user) {
+            System.out.println(s);
+        }
+    }
+
+    private static void twitterTest5() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter the name of the person below:");
+        String name = in.readLine();
+        ArrayList<String> location = new ArrayList<>(TwitterApi.getRecentLocation(name));
+        for (String s : location) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static void twitterTest6() throws IOException {
+
+        //ArrayList<String> location = new ArrayList<>(TwitterApi.getTrending());
+        ArrayList<String> location = new ArrayList<>(TwitterApi.getTrendingLocation("Auburn, AL"));
+        for (String s : location) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static void twitterTest7() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Put in the hashtag below:");
+        String name = in.readLine();
+        ArrayList<String> location = new ArrayList<>(TwitterApi.getTweets(name));
+        for (String s : location) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static void storeAccessToken(long useId, AccessToken accessToken){
+        //store accessToken.getToken()
+        //store accessToken.getTokenSecret()
     }
 }
---
+++ Scraper/src/me/williamhester/network/TwitterApi.java
@@ -0,0 +1,253 @@
+package me.williamhester.network;
+
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Mac on 2/13/16.
+ */
+public class TwitterApi {
+    private static AccessToken token = new AccessToken("", "");
+
+    public static List<String> getUser(String name) {
+        ArrayList<String> userList = new ArrayList<>();
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            User owner = twitter.verifyCredentials();
+
+            ResponseList<User> users = twitter.searchUsers("Johnny Appleseed", 1);
+            if (users.size() > 0) {
+                userList.add("Here are a few profiles matching \""  + name + "\".");
+                for (User u : users) {
+                    userList.add(u.getBiggerProfileImageURL());
+                    userList.add(u.getName());
+                    userList.add(u.getScreenName());
+                    userList.add(u.getLocation());
+                    userList.add("");
+                }
+            }
+            return userList;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        if (userList.size() == 0) {
+            userList.add("Sorry, I couldn't find any users with that name.");
+        }
+        return userList;
+    }
+
+    public static List<String> findBirthday(String accountName) {
+        ArrayList<String> birthday = new ArrayList<>();
+        try {
+            boolean found = false;
+            Twitter twitter = TwitterFactory.getSingleton();
+            User user = twitter.users().showUser(accountName);
+            birthday.add(user.getBiggerProfileImageURL());
+            if (user != null) {
+                if (!user.isProtected()) {
+                    int i = 0;
+                    while (true) {
+                        i++;
+                        ResponseList<Status> statuses = twitter.getUserTimeline(accountName, new Paging(i, 200));
+                        for (Status s : statuses) {
+                            if (!s.isRetweet()) {
+                                if (s.getText().toLowerCase().contains("my birthday") && s.getText().toLowerCase().contains("today")) {
+                                    String date = s.getCreatedAt().toString();
+                                    StringTokenizer split = new StringTokenizer(date);
+                                    String out = "";
+                                    split.nextToken();
+                                    out += split.nextToken() + " ";
+                                    out += split.nextToken();
+                                    out += " is " + s.getUser().getName() + "'s birthday!";
+                                    birthday.add(out);
+                                    found = true;
+                                    break;
+                                } else if (s.getText().toLowerCase().contains("my birthday") && s.getText().toLowerCase().contains("tomorrow")) {
+                                    String date = s.getCreatedAt().toString();
+                                    StringTokenizer split = new StringTokenizer(date);
+                                    String out = "";
+                                    split.nextToken();
+                                    out += split.nextToken() + " ";
+                                    out += (Integer.parseInt(split.nextToken()) + 1);
+                                    out += " is " + s.getUser().getName() + "'s birthday!";
+                                    birthday.add(out);
+                                    found = true;
+                                    break;
+                                } else if (s.getText().toLowerCase().contains("my birthday")) {
+                                    birthday.add("I couldn't find the exact birthday, but here are a few tweets that might be relevent:");
+                                    birthday.add("@" + s.getUser().getScreenName() + " " + s.getText() + " " + s.getCreatedAt().toString());
+                                }
+                            }
+                        }
+                        if (found || statuses.size() == 0 || i > 15) {
+                            break;
+                        }
+
+                    }
+                    if (birthday.size() < 2) {
+                        birthday.add("Sorry, but I couldn't find " + accountName + "'s birthday.");
+                    }
+                } else {
+                    birthday.add("I'm sorry, but " + user.getName() + "'s account is protected.");
+                }
+            } else {
+                birthday.add("I'm sorry, but " + accountName + " doesn't exist.");
+            }
+            return birthday;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return birthday;
+    }
+
+    public static List<String> getRecentLocation(String accountName) {
+        ArrayList<String> location = new ArrayList<>();
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            User user = twitter.users().showUser(accountName);
+            location.add(user.getBiggerProfileImageURL());
+            if (user != null) {
+                if (!user.isProtected()) {
+                    int i = 0;
+                    boolean found = false;
+                    while (true) {
+                        i++;
+                        ResponseList<Status> statuses = twitter.getUserTimeline(accountName, new Paging(i, 200));
+                        for (Status s : statuses) {
+                            if (s.getPlace() != null) {
+                                found = true;
+                                location.add(s.getUser().getName() + "'s most recent location is " + s.getPlace().getFullName() + ".");
+                                break;
+                            }
+                        }
+                        if (found || i > 20) {
+                            break;
+                        }
+                    }
+                    if (location.size() < 2) {
+                        location.add("I'm sorry, I could'nt find " + accountName + "'s most recent location.");
+                    }
+                } else {
+                    location.add("I'm sorry, but " + user.getName() + "'s account is protected.");
+                }
+            } else {
+                location.add("I'm sorry, but " + accountName + " doesn't exist.");
+            }
+        } catch (TwitterException e) {
+            if (location.size() == 0) {
+                location.add("I'm sorry, I could'nt find " + accountName + "'s most recent location.");
+            }
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    public static void getLimit() {
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            HashMap<String, RateLimitStatus> status = new HashMap<>(twitter.getRateLimitStatus());
+            for (String s : status.keySet()) {
+                System.out.println(s);
+                System.out.println(status.get(s).getRemaining() + "");
+                System.out.println(status.get(s).getSecondsUntilReset() + "");
+                System.out.println("");
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setAccount() {
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer("", "");
+        twitter.setOAuthAccessToken(token);
+
+    }
+
+    public static List<String> getTrending() {
+        ArrayList<String> trending = new ArrayList<>();
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            Trends trends = twitter.trends().getPlaceTrends(1);
+            int i = 0;
+            trending.add("*Here's what is trending globally:*");
+            trending.add("");
+            for (Trend t : trends.getTrends()) {
+                trending.add(t.getName());
+                trending.add(t.getURL());
+                trending.add("");
+                i++;
+                if (i == 10) {
+                    break;
+                }
+            }
+        } catch (TwitterException e) {
+
+        }
+        return trending;
+    }
+
+    /*public static List<String> getTrendingLocation(String city) {
+        ArrayList<String> trending = new ArrayList<>();
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            GeoLocation location = null;
+            int id = 0;
+            query.setQuery(city);
+            query.setMaxResults(5);
+            ResponseList<Place> places = twitter.searchPlaces(query);
+            for (Place p : places) {
+                if (p.getFullName().toLowerCase().contains(city.toLowerCase())) {
+                    GeoLocation[][] geolocation = p.getGeometryCoordinates();
+                    location = new GeoLocation()
+                }
+            }
+            int i = 0;
+            trending.add("*Here's what is trending in " + city + ":*");
+            trending.add("");
+            Trends trends = twitter.trends().getClosestTrends();
+            System.out.println(trends.toString());
+            for (Trend t : trends.getTrends()) {
+                trending.add(t.getName());
+                trending.add(t.getURL());
+                trending.add("");
+                i++;
+                if (i == 10) {
+                    break;
+                }
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return trending;
+    }*/
+
+    public static List<String> getTweets(String hashtag) {
+        ArrayList<String> tweets = new ArrayList<>();
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            Query query = new Query("");
+            query.setCount(10);
+            query.setQuery("#" + hashtag);
+            List<Status> list = twitter.search(query).getTweets();
+            if (list.size() > 0) {
+                tweets.add("*Here are a few tweets involving #" + hashtag+ ":*");
+                tweets.add("");
+                for (Status s : list) {
+                    tweets.add("@" + s.getUser().getName() + " " + s.getText());
+                    tweets.add(s.getCreatedAt().toString());
+                    tweets.add("");
+                }
+            }
+        } catch (TwitterException e) {}
+        if (tweets.size() == 0) {
+            tweets.add("Sorry, but I couldn't find any tweets for #" + hashtag + ".");
+        }
+        return tweets;
+    }
+}
